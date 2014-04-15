820767428076cbd8ad2475cf6d5e1a7d69fcbb50
BraydenYates <braydengyates@aol.com>
2014-04-15 15:51:21 -0500

    Update Init.cpp

--- src/Init.cpp
+++ src/Init.cpp
@@ -1,3 +1,31 @@
 #include <SDL/SDL.h>
 #include <iostream>
 #include <string>
+
+class Initial {
+  public:
+    void create() {
+      SDL_Init(SDL_INIT_EVERYTHING);
+      std::cout << "Tanner is your mom" << std::endl;
+    }
+    
+    void mainLoop() {
+      //Create global stuff 
+      SDL_Event *event;
+      SDL_Surface *buffer;
+      
+      while(event.type != SDL_QUIT)
+      {
+        /*************
+         * Order of Operations:
+         * Poll Event via SDL_PollEvent( event );
+         * if else statements
+         * Update background
+         * Blit background && other imgs to buffer
+         * SDL_Flip buffer;
+         * ***************/
+         
+      }
+      
+    }
+};
